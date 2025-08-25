3d861be1f79a1c5566af9d45893ba7062d4dbc45
leravvv <valeria.viktor@yandex.by>
2025-08-25 23:35:11 +0300

    GraphQL_FileStorage

---
+++ Computer_Systems_and_Networks/GraphQL_FileStorage/GraphQLFileStorage/FileContentType.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphQLFileStorage
+{
+    //определение типа содержимого файла
+    public class FileContent
+    {
+        public string Path { get; set; }
+        public string Content { get; set; }
+    }
+    //метод для построения схемы GraphQL
+    public class FileContentType : ObjectType<FileContent>
+    {
+        protected override void Configure(IObjectTypeDescriptor<FileContent> descriptor)
+        {
+            descriptor.Field(f => f.Path).Type<StringType>(); //включение поля пути в граф
+            descriptor.Field(f => f.Content).Type<StringType>();
+        }
+    }
+}
